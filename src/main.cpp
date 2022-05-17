fb35d20d57f5d26599b719b4844d4f625d19a99c
JagubSmof <smithjacob003@gmail.com>
2022-05-17 15:17:04 +0100

    Initialise window and working with file structure

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,24 @@
-#include <iostream>
+#include "raylib.h"
+
 int main()
 {
-    std::cout << "Hello World\n";
+    // initialise window
+    const int windowWidth{800};
+    const int windowHeight{600};
+    InitWindow(windowWidth, windowHeight, "Flappy Bird Clone");
+
+    // initialise target framerate
+    const int targetFramerate{60}; // 60 fps target
+    SetTargetFPS(targetFramerate);
+
+    // game loop
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+        ClearBackground(WHITE);
+        // game logic
+
+        EndDrawing();
+    }
+    CloseWindow();    
 }
